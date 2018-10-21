63f36734ef5f13dfd056445931b496b4a14fd14a
Danila <luckerze@yandex.ru>
2018-10-21 18:12:12 +0300

    first

---
+++ rogalik/Roguelike(Vers 1.0)/Assets/Skripts/Ilya/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Joystick joystick;
+    public float maxSpeed = 1.0f;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        Vector3 moveVector = (Vector3.right * joystick.Horizontal * maxSpeed + Vector3.up * joystick.Vertical * maxSpeed) * maxSpeed;
+
+        if (moveVector != Vector3.zero)
+        {
+            transform.Translate(moveVector * maxSpeed * Time.deltaTime, Space.World);
+        }
+    }
+}
